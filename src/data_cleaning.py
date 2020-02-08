babb26fc1d44dd3565318cc9a0ab3eddab3c6ee6
David <mwikyakimolo@gmail.com>
2020-02-08 13:21:28 +0300

    created the checking for nans function

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -7,7 +7,15 @@ cancellation_data_path = "resources/Passenger_Cancellations_Nairobi.csv"
 dataset = pd.read_csv(cancellation_data_path)
 
 # This function checks for NaN values in a given column
-
-
+def check_nans(column_name):
+    """ this function will take a column  and check for any available NaNs"""
+    all_nan_column_results =  dataset[column_name].isnull()
+    null_entries = []
+    for row in all_nan_column_results:
+        if row == True:
+            # if there is a nan entry, we append it to null_entries
+            null_entries.append(null_entries)
+    return null_entries
 
 # check for NaN values in trip_id column 
+print(check_nans("trip_id"))
