716910d741a07180d35d0330491020d85661648f
David <mwikyakimolo@gmail.com>
2020-02-08 12:58:10 +0300

    initial commit

---
+++ src/data_cleaning.py
@@ -0,0 +1,13 @@
+# imports
+import pandas as pd
+import numpy as np
+
+# dataset
+cancellation_data_path = "resources/Passenger_Cancellations_Nairobi.csv"
+dataset = pd.read_csv(cancellation_data_path)
+
+# This function checks for NaN values in a given column
+
+
+
+# check for NaN values in trip_id column 
